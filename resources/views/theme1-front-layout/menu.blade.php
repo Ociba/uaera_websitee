ad3d42c3de202ac513ddfa0bd5bedd49fe3ee157
Ociba James <ocibajames@gmail.com>
2021-06-23 13:54:23 +0300

    removed themes from menu

--- resources/views/theme1-front-layout/menu.blade.php
+++ resources/views/theme1-front-layout/menu.blade.php
@@ -13,24 +13,7 @@
         <li><a class="nav-link   scrollto" href="#portfolio">Team</a></li>
         <li><a class="nav-link scrollto" href="#team">Careers</a></li>
         <li><a class="nav-link scrollto" href="#contact">List of Uaera Members</a></li>
-        <li class="dropdown"><a href="#"><span>Themes</span> <i class="bi bi-chevron-down"></i></a>
-        <ul>
-            <li><a href="/theme-two">Theme2</a></li>
-            <li><a href="/theme-three">Theme3</a></li>
-            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-            <ul>
-                <li><a href="#">Deep Drop Down 1</a></li>
-                <li><a href="#">Deep Drop Down 2</a></li>
-                <li><a href="#">Deep Drop Down 3</a></li>
-                <li><a href="#">Deep Drop Down 4</a></li>
-                <li><a href="#">Deep Drop Down 5</a></li>
-            </ul>
-            </li>
-            <li><a href="#">Drop Down 2</a></li>
-            <li><a href="#">Drop Down 3</a></li>
-            <li><a href="#">Drop Down 4</a></li>
-        </ul>
-        </li>
+        
         <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
     </ul>
     <i class="bi bi-list mobile-nav-toggle"></i>
